ca18a89ea3c4667e7c6283041902a3f3eb7b7d92
peirong.wu <wupeirong294@gmail.com>
2019-12-28 17:29:46 +0800

    configure tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+var tailwindcss = require('tailwindcss');
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss'),
+    tailwindcss('./app/javascript/components/App.css'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
